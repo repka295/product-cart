83fa9699d91091df2f962732f201be8b772e42f1
david16-rgb <davidmardanov99@gmail.com>
2026-08-05 21:50:46 +0300

    Homework 07: functions and conditions

---
+++ homework-7.js
@@ -0,0 +1,23 @@
+function weather(city, temperature) {
+console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
+}
+
+const LIGHT_SPEED = 299792458;
+function checkSpeed(speed) {
+if (speed > LIGHT_SPEED) {
+        console.log("Сверхсветовая скорость");
+    } else if (speed < LIGHT_SPEED) {
+        console.log("Субсветовая скорость");
+    } else {
+        console.log("Скорость света");
+    }
+
+    const product = "Груши";
+const price = 100;
+function buy(budget) {
+if (budget >= price)
+    console.log(`${product} приобретён. Спасибо за покупку!`);
+if (budget < price)
+    const difference = price - budget;
+    console.log(`Вам не хватает ${difference}$, пополните баланс`);
+}
