94bc67632c38e0a2a6271a9c1c149935048328aa
david16-rgb <davidmardanov99@gmail.com>
2026-08-17 14:38:35 +0300

    feature/7-homework

--- homework-7.js
+++ homework-7.js
@@ -1,23 +1,30 @@
-function weather(city, temperature) {
-console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
+function getWeather(city, temperature) {
+    console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
 }
 
 const LIGHT_SPEED = 299792458;
 function checkSpeed(speed) {
-if (speed > LIGHT_SPEED) {
+    if (speed > LIGHT_SPEED) {
         console.log("Сверхсветовая скорость");
     } else if (speed < LIGHT_SPEED) {
         console.log("Субсветовая скорость");
     } else {
         console.log("Скорость света");
     }
+}
 
-    const product = "Груши";
+const product = "Груши";
 const price = 100;
 function buy(budget) {
-if (budget >= price)
-    console.log(`${product} приобретён. Спасибо за покупку!`);
-if (budget < price)
-    const difference = price - budget;
+    if (budget >= price)
+        console.log(`${product} приобретён. Спасибо за покупку!`);
+    if (budget < price)
+        const difference = price - budget;
     console.log(`Вам не хватает ${difference}$, пополните баланс`);
 }
+function sayHello() {
+    console.log("Привет!");
+}
+const name = "George";
+let age = 17;
+const city = "Moscow";
